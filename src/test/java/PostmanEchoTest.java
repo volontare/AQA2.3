06149c31792589ab37d61b93ba502773347afa93
Екатерина Сочкова <volontare@yandex.ru>
2021-05-20 22:22:19 +0400

    Test failed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,24 +1,19 @@
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
 
 public class PostmanEchoTest {
     @Test
     void shouldReturnData(){
-        // Given - When - Then
-// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello1")
                 .when()
                 .post("/post")
-// Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("Hello"));
         ;
     }
 }
